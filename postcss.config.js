38138f815527927608266734f3916bf386dfd8c8
Jonathan Harrell <harr041@gmail.com>
2017-11-02 14:53:27 -0500

    refactor: using postcss custom selectors

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,9 @@ module.exports = {
     plugins: [
         require('postcss-import')(),
         require('postcss-mixins')(),
+        require('postcss-custom-selectors')(),
         require('postcss-nesting')(),
+        require('autoprefixer')(),
         require('postcss-custom-media')()
     ]
 }
