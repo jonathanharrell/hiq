fe25860c7f2a52ed10f195b5f82b7e4d61416bd2
Jonathan Harrell <harr041@gmail.com>
2017-11-06 12:15:55 -0600

    refactor: splitting out pcss files more to be more atomically organized

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     plugins: [
-        require('postcss-import')(),
-        require('postcss-mixins')(),
-        require('postcss-custom-selectors')(),
-        require('postcss-nesting')(),
-        require('autoprefixer')(),
-        require('postcss-custom-media')(),
-        require('postcss-discard-comments')()
+        require('postcss-import'),
+        require('postcss-mixins'),
+        require('postcss-custom-selectors'),
+        require('postcss-nesting'),
+        require('autoprefixer'),
+        require('postcss-custom-media'),
+        require('postcss-discard-comments')
     ]
 }
